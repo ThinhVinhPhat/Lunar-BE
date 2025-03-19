d4ef9c383d9082a416f6500dec7cbdd7ee7ac541
ThinhVinhPhat <thin[hvinhp@gmail.com>
2025-03-19 23:31:05 +0700

    update user update DTO

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,59 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiProperty({
+    description: 'first name',
+    example: 'Nguyễn Văn',
+    nullable: false,
+  })
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty({
+    description: 'last name',
+    example: 'Tèo',
+    nullable: false,
+  })
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({
+    description: 'user phone',
+    example: '0902423699',
+    nullable: false,
+  })
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @ApiProperty({
+    description: 'user current city',
+    example: 'Hanoi',
+    nullable: false,
+  })
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @ApiProperty({
+    description: 'user address',
+    example: '123 Hai Bà Trưng',
+    nullable: false,
+  })
+  @IsString()
+  @IsOptional()
+  address: string;
+
+  @ApiProperty({
+    description: 'user address',
+    example: 'Công ty ACB',
+    nullable: false,
+  })
+  @IsString()
+  @IsOptional()
+  company: string;
+}
