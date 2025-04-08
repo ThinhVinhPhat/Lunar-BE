22678d8c15b8b9167f44a77c4f73507ed066bc39
ThinhVinhPhat <thin[hvinhp@gmail.com>
2025-04-08 14:43:21 +0700

    fix typeorm

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -18,9 +18,9 @@ export const config = {
   autoLoadEntities: true,
   synchronize: false,
   logging: process.env?.DATABASE_DEBUG_MODE === 'true' || false,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 if (config.logging) {
   console.log('Connect DB Successfully');
