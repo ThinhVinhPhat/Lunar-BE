2dff37d7cf138ce9b3af622166f5bfeff9153a78
ThinhVinhPhat <thin[hvinhp@gmail.com>
2025-04-24 16:34:28 +0700

    add middleware

--- apps/webhook/src/main.ts
+++ apps/webhook/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { WebhookModule } from './webhook.module';
+import rawBodyMiddleware from './domain/middleware/raw-body.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(WebhookModule);
   app.setGlobalPrefix('api/v1/webhook');
+  app.use(rawBodyMiddleware());
+
   await app.listen(3000);
 }
 bootstrap();
